bdc9b64499c75ef67048f9222eebddbcfdc0968a
nts-xx <nts-xx@users.noreply.github.com>
2014-10-25 17:53:47 +0200

    bugfix - class class is illegal

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
+   
     var random = Math.floor(Math.random() * 100);
     var desicion = "";
     var color = "";
-    var class = "";
+    var divclass = "";
+    
     if((random  >= 0) && (random <= 39)) {
     desicion = "FOR";
     color = "green";
-    class = "";
+    divclass = "";
     }
     else if((random >= 40) && (random <=79)) {
     desicion = "MOT";
     color = "red";
-    class = "";
+    divclass = "";
     }
     else if((random >= 80) && (random <=95)) {
     desicion = "AVHOLDENDE";
     color = "yellow";
-    class = "";
+    divclass = "";
     }
     else {
     desicion = "IKKE, men gir ordstyrer Inge en klem";
     color = "hotpink";
-    class = "font-effect-neon"
+    divclass = "font-effect-neon";
     }
-    $("#result").html(desicion).css( "color", color ).attr("class", class);
+    $("#result").html(desicion).css( "color", color ).attr("class", divclass);
 
     $.getJSON("/js/data.json", function (data) {
         console.log(data);
