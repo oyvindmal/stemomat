ac9ee122cfbe8ee8ef80534f050198b6d6e5f0b6
nts-xx <nts-xx@users.noreply.github.com>
2014-10-25 17:43:03 +0200

    Google-font effect prep + Inge-neon-class

--- js/script.js
+++ js/script.js
@@ -2,28 +2,30 @@ $(document).ready(function () {
     var random = Math.floor(Math.random() * 100);
     var desicion = "";
     var color = "";
+    var class = "";
     if((random  >= 0) && (random <= 39)) {
     desicion = "FOR";
     color = "green";
+    class = "";
     }
     else if((random >= 40) && (random <=79)) {
     desicion = "MOT";
     color = "red";
+    class = "";
     }
     else if((random >= 80) && (random <=95)) {
     desicion = "AVHOLDENDE";
     color = "yellow";
+    class = "";
     }
     else {
     desicion = "IKKE, men gir ordstyrer Inge en klem";
     color = "hotpink";
+    class = "font-effect-neon"
     }
-    $("#result").html(desicion).css( "color", color ); 
+    $("#result").html(desicion).css( "color", color ).attr("class", class);
 
     $.getJSON("/js/data.json", function (data) {
         console.log(data);
     })
-
-
 });
-
