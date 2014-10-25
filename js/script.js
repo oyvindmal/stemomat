aa3dbccf7bb71fdf4dd63793c0b3b930d0d09e48
Lars Åge Kamfjord <lars-github@kamfjord.org>
2014-10-25 15:29:43 +0200

    Nytt alternativ...

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(document).ready( function () {
 	var random = Math.floor(Math.random() * 100);
 	var desicion = "";
-	if((random  >= 0) && (random <= 44)) {
+	if((random  >= 0) && (random <= 39)) {
 		desicion = "FOR";
 	}
-	else if((random >= 45) && (random <=90)) {
+	else if((random >= 40) && (random <=79)) {
 		desicion = "MOT";
 	}
-	else if(random >= 91) {
+	else if((random >= 80) && (random <=95)) {
 		desicion = "AVHOLDENDE";
+	}
+	else {
+		desicion = "IKKE, men gir ordstyrer Inge en klem";
 	}
 	$("#result").html(desicion);
 });
