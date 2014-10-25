d9c5d5ec72f2abb9237aadad7ea08a9cde27d5a3
Øyvind Malin <oyvindmal@outlook.com>
2014-10-25 13:58:08 +0200

    Added basic voting logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(document).ready( function () {
-	var random = Math.floor((Math.random() * 2) + 1);;
-	console.log(random)
+	var random = Math.floor((Math.random() * 2) + 1);
+	var desicion = "";
+	if(random === 1) {
+		desicion = "FOR";
+	}
+
+	else {
+		desicion = "MOT";
+	}
+	$("#result").html(desicion);
 });
