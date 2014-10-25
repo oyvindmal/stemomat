13d4dcb82cde6fa5c5a67e376ac3ef62150dc362
oyvindmal <oyvindmal@outlook.com>
2014-10-25 16:07:44 +0200

    Started work on json data file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-$(document).ready( function () {
-	var random = Math.floor(Math.random() * 100);
-	var desicion = "";
-	if((random  >= 0) && (random <= 39)) {
-		desicion = "FOR";
-	}
-	else if((random >= 40) && (random <=79)) {
-		desicion = "MOT";
-	}
-	else if((random >= 80) && (random <=95)) {
-		desicion = "AVHOLDENDE";
-	}
-	else {
-		desicion = "IKKE, men gir ordstyrer Inge en klem";
-	}
-	$("#result").html(desicion);
+$(document).ready(function () {
+    var random = Math.floor(Math.random() * 100);
+    var desicion = "";
+    if((random  >= 0) && (random <= 39)) {
+    desicion = "FOR";
+    }
+    else if((random >= 40) && (random <=79)) {
+    desicion = "MOT";
+    }
+    else if((random >= 80) && (random <=95)) {
+    desicion = "AVHOLDENDE";
+    }
+   
+    else {
+    desicion = "IKKE, men gir ordstyrer Inge en klem";
+    }
+    $("#result").html(desicion); 
+
+    $.getJSON("/js/data.json", function (data) {
+        console.log(data);
+    })
+
+
 });
 
