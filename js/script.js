619cd0e6c7af3d3b5aa0c43f320c7aa177e138c7
nts-xx <nts-xx@users.noreply.github.com>
2014-10-25 16:40:16 +0200

    Colors!

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function () {
     var random = Math.floor(Math.random() * 100);
     var desicion = "";
+    var color = "";
     if((random  >= 0) && (random <= 39)) {
     desicion = "FOR";
+    color = "green";
     }
     else if((random >= 40) && (random <=79)) {
     desicion = "MOT";
+    color = "red";
     }
     else if((random >= 80) && (random <=95)) {
     desicion = "AVHOLDENDE";
+    color = "yellow";
     }
-   
     else {
     desicion = "IKKE, men gir ordstyrer Inge en klem";
+    color = "hotpink";
     }
-    $("#result").html(desicion); 
+    $("#result").html(desicion).css( "color", color ); 
 
     $.getJSON("/js/data.json", function (data) {
         console.log(data);
