16027a51575e35884f900f05765b99f3eb243bb8
nts-xx <nts-xx@users.noreply.github.com>
2014-10-26 12:57:51 +0100

    animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).keypress(function () {
    
     var random = Math.floor(Math.random() * 100);
     var desicion = "";
@@ -8,12 +8,12 @@ $(document).ready(function () {
     if((random  >= 0) && (random <= 39)) {
     desicion = "FOR";
     color = "green";
-    divclass = "";
+    divclass = "animated flash";
     }
     else if((random >= 40) && (random <=79)) {
     desicion = "MOT";
     color = "red";
-    divclass = "";
+    divclass = "animated shake";
     }
     else if((random >= 80) && (random <=95)) {
     desicion = "AVHOLDENDE";
@@ -23,7 +23,7 @@ $(document).ready(function () {
     else {
     desicion = "IKKE, men gir ordstyrer Inge en klem";
     color = "hotpink";
-    divclass = "font-effect-neon";
+    divclass = "font-effect-neon animated flip";
     }
     $("#result").html(desicion).css( "color", color ).attr("class", divclass);
 
