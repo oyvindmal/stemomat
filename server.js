62b55177e8de1c3c08ade34bcccdeb4ffd6f6a65
Øyvind Malin <oyvindmal@outlook.com>
2015-10-24 11:37:49 +0200

    Fixed it

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var http = require('http').Server(app);
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});*/
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
