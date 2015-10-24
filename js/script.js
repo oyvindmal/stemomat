71e211e9c733cc79db70b88701abf74f92c9600d
Øyvind Malin <oyvindmal@outlook.com>
2015-10-24 11:57:56 +0200

    La til tilgjengelighetsknappen

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
 $(document).keypress(function () {
-   
-    var random = Math.floor(Math.random() * 100);
-    var desicion = "";
-    var color = "";
-    var divclass = "";
-    
-    if((random  >= 0) && (random <= 39)) {
-    desicion = "FOR";
-    color = "green";
-    divclass = "animated flash";
-    }
-    else if((random >= 40) && (random <=79)) {
-    desicion = "MOT";
-    color = "red";
-    divclass = "animated shake";
-    }
-    else if((random >= 80) && (random <=95)) {
-    desicion = "AVHOLDENDE";
-    color = "yellow";
-    divclass = "";
-    }
-    else {
-    desicion = "IKKE, men gir ordstyrer Inge en klem";
-    color = "hotpink";
-    divclass = "font-effect-neon animated flip";
-    }
-    $("#result").html(desicion).css( "color", color ).attr("class", divclass);
+    doMagic();
 
-    $.getJSON("/js/data.json", function (data) {
-        console.log(data);
-    })
 });
+
+$("#btnStart").click(function () {
+  doMagic();
+})
+
+function doMagic()
+{
+  var random = Math.floor(Math.random() * 100);
+  var desicion = "";
+  var color = "";
+  var divclass = "";
+
+  if((random  >= 0) && (random <= 39)) {
+  desicion = "FOR";
+  color = "green";
+  divclass = "animated flash";
+  }
+  else if((random >= 40) && (random <=79)) {
+  desicion = "MOT";
+  color = "red";
+  divclass = "animated shake";
+  }
+  else if((random >= 80) && (random <=95)) {
+  desicion = "AVHOLDENDE";
+  color = "yellow";
+  divclass = "";
+  }
+  else {
+  desicion = "IKKE, men gir ordstyrer Inge en klem";
+  color = "hotpink";
+  divclass = "font-effect-neon animated flip";
+  }
+  $("#result").html(desicion).css( "color", color ).attr("class", divclass);
+
+
+}
